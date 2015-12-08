d4a46e75986f701a4b6236abd7b78e7b97d10a0f
An <an.ho1991@gmail.com>
2015-12-08 23:45:26 +0700

    Update new page

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
 $(document).ready(function() {
+	//Init WOW
+	new WOW().init();
+
+	//Navbar scroll effect
 	$(window).scroll(function() {
+	    var el = $('.navbar-default');
 	    if ($(window).width() > 991){
-			var el = $('.navbar-default');
 	    	var navHeight = el.outerHeight();
 	    	if ($(window).scrollTop() >= navHeight)
 	    		el.addClass('scrollDown');
 	    	else
 	    		el.removeClass('scrollDown');
 	    }
-	})	
+	    else
+	    	el.removeClass('scrollDown');
+
+	})
+
+	//Adjust qty
+	$('.qty-modifier span').on('click', function(){
+		//Get current value
+		var input = $(this).parents('.qty-container').find('input');
+		var currenValue = input.val();
+		//Detect Plus or Minus and Update qty
+		if ($(this).children('i').hasClass('fa-plus')){
+			var newValue = parseInt(currenValue) + 1;
+			if ($.isNumeric(newValue))
+				input.val(newValue);
+			else
+				input.val(1);
+		}
+		else{
+			var newValue = parseInt(currenValue) - 1;
+			if ( ($.isNumeric(newValue)) & newValue > 1)
+				input.val(newValue);
+			else
+				input.val(1);
+		}
+	});
 });
